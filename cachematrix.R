1d9c71e346279434f5f8d298f9d8701d4c542292
L.PENINGAULT <l.peningault@free.fr>
2015-05-24 23:20:06 +0200

    assignement 2 done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  b <- FALSE               # status of cache is True if Inverse matrix has been cached 
+  i <- matrix()            # i : Inverse Matrix
+  
+  set <- function(y) {     # set stores matrix and boolean of Inverse matrix at the
+    x <<- y                # scope of the main function 
+    b <<- FALSE
+  }
+  get <- function() x      # get returns the matrix of the main function
+  
+  setinv <- function(inverse)  # setinv stores the inverse matrix and its boolean
+    { i <<- inverse            # at the scope of main function
+      b <<- TRUE }
+  
+  getinv <- function() {      # getinv returns the matrix of inverse matrix and its boolean      
+    list(b,i) }               # as a list
+  
+  list(set = set, get = get,  # list of 4 returned by the main function
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  cache <- x$getinv()   # reading of the object created with makeCacheMatrix 
+  if(cache[[1]]) {      # check of the cache status (boolean), cached=True
+    message("getting cached data")
+    return(cache[[2]])  # output of inverse matrix 
+                        # cache[[2]] is the inverse matrix cached
+  }
+  mat <- x$get()        # reading of matrix stored in object created with makeCacheMatrix
+  i <- solve(mat)       # computation of inverse matrix 
+  x$setinv(i)           # result is cached
+  i                     # output of inverse matrix 
 }
